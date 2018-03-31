f4cb55ddb9bacbb162e94f262054f4fdb86c4ccb
Hermeto Romano <hermeto.romano@gmail.com>
2018-03-31 00:13:54 -0300

    refactored index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,33 +1,18 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <title>GClub - Challenge</title>
-</head>
-<body>
-<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-    <h1 class="display-4">Gamers Club Challenge</h1>
-</div>
-<div class="container">
-    <form method="post" action="/start">
-        @csrf
-        <div class="col-4">
-            <button type="submit" class="btn btn-lg btn-block btn-primary">Start</button>
-        </div>
-        <footer class="pt-4 my-md-5 pt-md-5 border-top">
-            <div class="col-6 col-md text-center">
-                <ul class="list-unstyled text-small">
-                    <li><a class="text-muted" href="#">Desafio</a></li>
-                    <li><a class="text-muted" href="#">Código fonte</a></li>
-                </ul>
-            </div>
-        </footer>
-    </form>
-</div>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-</body>
-</html>
+@extends('main')
+@section('content')
+    <div class="text-center">
+        <h1 class="display-4">Gamers Club Challenge</h1>
+        <form method="post" action="/start">
+            @csrf
+            <button type="submit" class="btn btn-lg* btn-block btn-primary">Start</button>
+            <footer class="pt-4 my-md-5 pt-md-5 border-top">
+                <div class="col-6 col-md text-center">
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-muted" href="#">Desafio</a></li>
+                        <li><a class="text-muted" href="#">Código fonte</a></li>
+                    </ul>
+                </div>
+            </footer>
+        </form>
+    </div>
+@stop
